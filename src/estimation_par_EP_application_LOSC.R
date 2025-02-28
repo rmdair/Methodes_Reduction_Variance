3ee1f5fad5cf3ffc662784ed1809bf70fe7a6a91
Reda Mdair <redamdair@outlook.com>
2025-02-28 22:12:28 +0100

    Update estimation_par_EP_application_LOSC.R

--- src/estimation_par_EP_application_LOSC.R
+++ src/estimation_par_EP_application_LOSC.R
@@ -1,21 +1,22 @@
 set.seed(4)
 
-data <- read.csv2("https://rmdair.com/data/LOSC.csv", header=F, stringsAsFactors = FALSE, na.strings = c(""))
+data <- read.csv2("directory_name/LOSC.csv", header=F, 
+       stringsAsFactors = FALSE, na.strings = c(""))
 X <- as.vector(as.matrix(data))
 X <- X[!is.na(X)]
 
 n <- length(X)
 mu <- mean(X)
 sigma <- sd(X)
-N <- (X-mu) / sigma
+N <- (X - mu) / sigma
 
-valeur_exacte <- 1-(pnorm(4.663381, 0, 1))
+exact_value <- 1 - (pnorm(4.663381, 0, 1))
 
-MCC <- mean(ifelse(X >= 8, 1, 0))
+MC_estimator <- mean(ifelse(X >= 8, 1, 0)) 
 
-Y <- rt(n, 2)
-w <- dnorm(Y) / dt(Y,2)
-EP <- mean(w * ifelse(Y >= 4.663381, 1, 0))
+Y <- rt(n, 2)     
+w <- dnorm(Y) / dt(Y, 2)
+IS_estimator <- mean(w * ifelse(Y >= 4.663381, 1, 0)) 
 
 ESS <- function(n) {
   sum_1 <- 0
@@ -27,11 +28,11 @@ ESS <- function(n) {
   return(sum_1^2 / sum_2)
 }
 
-cat("Estimation par MCC:", MCC)
-cat("Estimation par EP:", EP)
-cat("Taille effective ESS:", ESS(n))
+cat("Monte Carlo Estimation:", MC_estimator)
+cat("Importance Sampling Estimation:", IS_estimator)
+cat("Effective Sample Size (ESS):", ESS(n))
 
-var_MCC <- ((n-1)/n) * var(ifelse(N >= 4.663381, 1, 0))
-var_EP <- ((n-1)/n) * var(ifelse(Y >= 4.663381, 1, 0) * w)
-cat( "Variance par MCC:", var_MCC )
-cat( "Variance par EP:", var_EP )
+var_MC <- ((n-1)/n) * var(ifelse(N >= 4.663381, 1, 0))
+var_IS <- ((n-1)/n) * var(ifelse(Y >= 4.663381, 1, 0) * w)
+cat("Monte Carlo Variance:", var_MC)
+cat("Importance Sampling Variance:", var_IS)
