84fcf0961af4c5f9271727f1fd0cfab5ea8374ba
Reda Mdair <redamdair@outlook.com>
2025-02-28 22:07:50 +0100

    Update estimation_par_VC.R

--- src/estimation_par_VC.R
+++ src/estimation_par_VC.R
@@ -5,25 +5,24 @@ f <- function(u) {
 }
 
 monte_carlo <- function(sample, n) {
-  moyenne <- mean(sample)
-  variance <- ((n-1)/n) * var(sample)
-  return(c(moyenne = moyenne, variance = variance)) 
+  mean_value <- mean(sample)
+  variance_value <- ((n-1)/n) * var(sample)
+  return(c(mean = mean_value, variance = variance_value)) 
 }
 
 n <- 10^4
 
 sample_U <- runif(n)
-sample_MCC <- f(sample_U)
-sample_VC1 <- f(sample_U) - (sample_U - 1/2)
-sample_VC2 <- f(sample_U) - (log(1 + 2*sample_U) - 0.6479184)
 
-tableau <- data.frame(
-  MCC = monte_carlo(sample_MCC, n),
-  VC1 = monte_carlo(sample_VC1, n),
-  VC2 = monte_carlo(sample_VC2, n)
+sample_MC <- f(sample_U)
+sample_CV1 <- f(sample_U) - (sample_U - 1/2)
+sample_CV2 <- f(sample_U) - (log(1 + 2*sample_U) - 0.6479184)
+
+result_table <- data.frame(
+  MC = monte_carlo(sample_MC, n),
+  CV1 = monte_carlo(sample_CV1, n),
+  CV2 = monte_carlo(sample_CV2, n)
 )
 
-rownames(tableau) <- c("Moyenne", "Variance")
-tableau
-
-
+rownames(result_table) <- c("Mean", "Variance")
+result_table
