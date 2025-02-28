4dbaf3912deb94c3f0744e3fb4bbd49f96e9b11c
Reda Mdair <redamdair@outlook.com>
2025-02-28 22:10:44 +0100

    Update estimation_par_ST.R

--- src/estimation_par_ST.R
+++ src/estimation_par_ST.R
@@ -1,31 +1,34 @@
-set.seed(1)
+set.seed(1) 
 
 n <- 10^4
-nb_strates <- 5
-valeurs_strates <- seq(0, 1, length.out = nb_strates + 1)
-ech_unif <- runif(n)
+num_strata <- 5
+strata_values <- seq(0, 1, length.out = num_strata + 1)
 
-f <- function(i) {
-  return( mean(ech_unif[ech_unif >= valeurs_strates[i]
-                        & ech_unif < valeurs_strates[i + 1]]) )
+sample_unif <- runif(n)
+
+compute_mean <- function(i) {
+  return(mean(sample_unif[sample_unif >= strata_values[i] 
+           & sample_unif < strata_values[i + 1]]))
 }
-g <- function(i) {
-  return( mean(ech_unif[ech_unif >= valeurs_strates[i]
-                        & ech_unif < valeurs_strates[i + 1]]^2) )
+compute_mean_sq <- function(i) {
+  return(mean(sample_unif[sample_unif >= strata_values[i] 
+         & sample_unif < strata_values[i + 1]]^2))
 }
 
-mean_strate <- sapply(1:nb_strates, function(i) f(i))
-mean_sq_strate <- sapply(1:nb_strates, function(i) g(i))
+mean_stratum <- sapply(1:num_strata, function(i) compute_mean(i))
+mean_sq_stratum <- sapply(1:num_strata, function(i) compute_mean_sq(i))
 
-estimateur_MCC <- sum(ech_unif) / n
-estimateur_ST <- sum((1/5) * mean_strate)
+estimator_MC <- sum(sample_unif) / n
+estimator_ST <- sum((1/5) * mean_stratum)
 
-variance_MCC <- (integrate(function(u) u^2 * dunif(u, 0, 1), -Inf, Inf)$value - (1/2)^2) / n
-variance_ST <- sum((1/5) * (mean_sq_strate - mean_strate^2)) / n
-variance_ST_minimale <- sum((1/5) * sqrt(mean_sq_strate - mean_strate^2))^2 / n
+variance_MC <- (integrate(function(u) u^2 * 
+        dunif(u, 0, 1), -Inf, Inf)$value - (1/2)^2) / n
+variance_ST <- sum((1/5) * (mean_sq_stratum - mean_stratum^2)) / n
+variance_ST_min <- sum((1/5) * sqrt(mean_sq_stratum - mean_stratum^2))^2 / n
 
-cat("Estimateur par MCC :", estimateur_MCC)
-cat("Estimateur par ST :", estimateur_ST)
-cat("Variance par MCC:", variance_MCC)
-cat("Variance par ST :", variance_ST)
-cat("Variance minimale par ST :", variance_ST_minimale)
+cat("Monte Carlo Estimator:", estimator_MC)
+cat("Stratified Sampling Estimator:", estimator_ST)
+
+cat("Monte Carlo Variance:", variance_MC)
+cat("Stratified Sampling Variance:", variance_ST)
+cat("Minimum Stratified Sampling Variance:", variance_ST_min)
